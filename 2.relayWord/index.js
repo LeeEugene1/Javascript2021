81ebff12d8815184de5f2c63cab837de02e7b5d7
LeeEugene1 <uwm1004@gmail.com>
2021-02-01 17:57:10 +0900

    word relay

--- 2.relayWord/index.js
+++ 2.relayWord/index.js
@@ -1,6 +1,21 @@
 document.querySelector('#button').addEventListener('click',()=>{
-    const word = document.querySelector('#word').textContent
-    const input = document.querySelector('#input').value
-    document.write(input.charAt(0));//첫번째문자
-    document.write(word.substr(str.length-1,1));//마지막문자
+    const word = document.querySelector('#word').textContent;
+    const input = document.querySelector('#input').value;
+    const word_last = word.charAt(word.length - 1);
+    const input_first = input.charAt(0);
+    // alert(input_first)
+    if(word_last === input_first){
+        // alert('동일')
+        document.querySelector('#word').textContent = document.querySelector('#input').value;
+        // input === null;
+        document.querySelector('#input').value = ''
+        document.querySelector('#error').textContent = ''
+        document.querySelector('#input').focus();
+    }else{
+        // alert('다시입력')
+        document.querySelector('#error').textContent = '땡'
+        document.querySelector('#input').value = ''
+        document.querySelector('#input').focus();
+    }
+    
 });
